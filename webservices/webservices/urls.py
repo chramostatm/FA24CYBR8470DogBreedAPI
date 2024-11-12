93f1651585a102be64ca297c5ae6ecc37946d4e6
chramostatm <tmchramosta@gmail.com>
2024-11-11 20:27:59 -0600

    refactored to standard

--- webservices/webservices/urls.py
+++ webservices/webservices/urls.py
@@ -22,7 +22,7 @@ from dogapi.views import BreedViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/dog', DogViewSet.as_view({'get, post':'list'})),#GET (list), POST
-    path('api/dog/<id>', DogViewSet.as_view({'get':'retrieve', 'put':'add', 'delete':'remove'})),#GET, PUT, DELETE
+    path('api/dog/<id>', DogViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),#GET, PUT, DELETE
     path('api/breeds', BreedViewSet.as_view({'get, post':'list'})),#GET (list), POST
-    path('api/dog/<id>', BreedViewSet.as_view({'get':'retrieve', 'put':'add', 'delete':'remove'})),#GET, PUT, DELETE
+    path('api/dog/<id>', BreedViewSet.as_view({'get':'retrieve', 'put':'update', 'delete':'destroy'})),#GET, PUT, DELETE
 ]
